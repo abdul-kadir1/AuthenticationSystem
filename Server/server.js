3d76467e17da1a397abc4cbc540b3c0e02073d7e
Abdul Kadir <abdulkadir80949@gmail.com>
2025-11-30 09:56:08 +0530

    ready to deploy

--- Server/server.js
+++ Server/server.js
@@ -5,10 +5,16 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js'; 
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const app = express();
+
+const _dirname = path.resolve();
 
 const port = process.env.PORT || 4000;
 connectDB();
@@ -22,11 +28,18 @@ app.use(cors({origin:allowedOrigins,credentials:true}))
 
 
 //API Endpoints................................
-app.get('/',(req,res)=>{
-  res.send("API Working")
-})
+// app.get('/',(req,res)=>{
+//   // res.send("API Working")
+// })
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter) 
+
+
+app.use(express.static(path.join(_dirname,"/Client/dist")))
+app.get(/.*/,(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"Client","dist","index.html"));
+}) 
+
 
 app.listen(port , ()=>{
   console.log(`Server started on PORT :${port}`)
